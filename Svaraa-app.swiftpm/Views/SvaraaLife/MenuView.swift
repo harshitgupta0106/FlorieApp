fb330b75ebaebd94e97dc8d09708dc67867412ef
harshitgupta0106 <smartgharshit@gmail.com>
2025-02-21 00:01:40 +0530

    Updated menu list

---
+++ Svaraa-app.swiftpm/Views/SvaraaLife/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Svaraa
+//
+//  Created by Harshit Gupta on 20/02/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
